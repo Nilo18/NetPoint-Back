d891e302bc2293e961469d6ccec3ce96927d8fde
Nilo18 <nikalongurashvili@gmail.com>
2026-06-03 18:34:01 +0400

    refactor: move changePlan() and cancelSubscription to PaymentPlanController

--- src/main/java/com/netpoint/main/controllers/PaymentPlanController.java
+++ src/main/java/com/netpoint/main/controllers/PaymentPlanController.java
@@ -2,15 +2,14 @@ package com.netpoint.main.controllers;
 
 import com.netpoint.main.dto.AuthenticatedUser;
 import com.netpoint.main.dto.PaymentPlanDTO;
+import com.netpoint.main.dto.requests.ChangePlanRequest;
 import com.netpoint.main.services.PaymentPlanService;
 import lombok.Data;
 import lombok.extern.java.Log;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Data
 @RestController
@@ -25,5 +24,20 @@ public class PaymentPlanController {
         Integer companyId = user.companyId().intValue();
         log.info("companyId IS: " + companyId);
         return ResponseEntity.ok(paymentPlanService.getPaymentPlan(companyId));
+    }
+
+    @PutMapping
+    @PreAuthorize("hasAuthority('OWNER')")
+    public ResponseEntity<String> changePlan(@RequestBody ChangePlanRequest request) {
+        // ეს შეცვალე რომ ახალ სტრუქტურას მოერგოს
+        return ResponseEntity.ok("Plan changed to: " + request.getNewPlanName());
+    }
+
+    // DELETE - საბსქრიბშინს შლის
+    @DeleteMapping
+    @PreAuthorize("hasAuthority('OWNER')")
+    public ResponseEntity<String> cancelSubscription(@AuthenticationPrincipal AuthenticatedUser user) {
+        // ეს შეცვალე რომ ახალ სტრუქტურას მოერგოს
+        return ResponseEntity.ok("Subscription cancelled successfully");
     }
 }
